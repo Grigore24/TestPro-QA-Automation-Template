f18a71511d0bbb0f4e0432a40372b7789e271b3e
Grigore24 <133910092+Grigore24@users.noreply.github.com>
2025-02-22 17:05:44 +0000

    Selenium1

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,6 +1,9 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.safari.SafariDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -19,7 +22,80 @@ public class LoginTests extends BaseTest {
 
         String url = "https://qa.koel.app/";
         driver.get(url);
+
+        WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
+        emailInput.click();
+        emailInput.clear();
+
+
+        WebElement passwordInput = driver.findElement(By.cssSelector("[type = 'password']"));
+        passwordInput.click();
+        passwordInput.clear();
+
+        WebElement loginButton = driver.findElement(By.cssSelector("[type='submit"));
+        loginButton.click();
+
+
         Assert.assertEquals(driver.getCurrentUrl(), url);
+        driver.quit();
+    }
+
+    @Test
+    public void loginSuccessTest() throws InterruptedException {
+
+//      Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        //WebDriver driver = new SafariDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+        WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
+        emailInput.click();
+        emailInput.clear();
+        emailInput.sendKeys("grigore.crepciuc@testpro.io");
+
+        WebElement passwordInput = driver.findElement(By.cssSelector("[type = 'password']"));
+        passwordInput.click();
+        passwordInput.clear();
+        passwordInput.sendKeys("te$t$tudent22");
+
+        WebElement loginButton = driver.findElement(By.cssSelector("[type='submit"));
+        loginButton.click();
+        WebElement avatar = driver.findElement((By.cssSelector(".avatar")));
+        Thread.sleep(5000);
+        Assert.assertTrue(avatar.isDisplayed());
+        driver.quit();
+    }
+    @Test
+    public void loginInvalidEmailValidPassword() {
+
+//      Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+        WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
+        emailInput.click();
+        emailInput.clear();
+        emailInput.sendKeys("pgrigore.crepciuc@testpro.io");
+
+        WebElement passwordInput = driver.findElement(By.cssSelector("[type = 'password']"));
+        passwordInput.click();
+        passwordInput.clear();
+        passwordInput.sendKeys("te$t$tudent22");
+
+        WebElement loginButton = driver.findElement(By.cssSelector("[type='submit"));
+        loginButton.click();
+
+        Assert.assertTrue(loginButton.isDisplayed());
         driver.quit();
     }
 }
