6ff6a7a97b60519f82e180da4a70a3b371a8615b
Grigore24 <133910092+Grigore24@users.noreply.github.com>
2026-04-15 21:56:53 +0100

    Lecture1TestPro

---
+++ src/main/java/HomeworksTestPro/Homework1.java
@@ -0,0 +1,10 @@
+package HomeworksTestPro;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        int a = 24;
+        double b = 2.0;
+        double c = a/b;
+        System.out.println(c);
+    }
+}
