9126737eb823f78884eca1db4b16f172b973183d
Grigore24 <133910092+Grigore24@users.noreply.github.com>
2026-04-17 15:39:26 +0100

    Lecture4

--- src/main/java/HomeworksTestPro/HWL4Loops.java
+++ src/main/java/HomeworksTestPro/HWL4Loops.java
@@ -1,4 +1,70 @@
 package HomeworksTestPro;
 
 public class HWL4Loops {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x <= 4) {
+            System.out.println("Value of x: " + x);
+            x++;
+        }
+        int y = 1;
+        int sum = 0;
+        while (y <= 100) {
+            sum = sum + y;
+            y = y + 1;
+        }
+        System.out.println(sum);
+
+        //int u = 1;
+        int summ = 0;
+        for (int u = 1; u <= 100; u++) {
+            summ = summ + u;
+        }
+        System.out.println(summ);
+
+        for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+                break;
+            }
+            System.out.println(i);
+        }
+        for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+                continue;
+            }
+            System.out.println(i);
+        }
+        int count = 0;
+        while (count < 150) {
+            System.out.print("a");
+            count++;
+        }
+        //Fibonachi
+        int n = 10;
+        int first = 0;
+        int second = 1;
+        int third = 0;
+        System.out.println(first);
+        System.out.println(second);
+        for (int mount = 0; mount < n; mount++) {
+            third = first + second;
+            System.out.println(third);
+            first = second;
+            second = third;
+        }
+        // Loop do while
+        int c = 1;
+        do {
+            System.out.println("a");
+            c++;
+        } while (c < 10);
+
+        //for each Loop
+        String[] daysOfWeek = {"Mon", "Tue","Wedn"};
+        for (String dayWeek : daysOfWeek){
+            System.out.println(dayWeek);
+        }
+    }
+    //перезапуск
+
 }
--- src/main/java/HomeworksTestPro/HomeworkL3.java
+++ src/main/java/HomeworksTestPro/HomeworkL3.java
@@ -1,4 +1,52 @@
 package HomeworksTestPro;
 
 public class HomeworkL3 {
+    public static void main(String[] args) {
+        int time =25;
+        if (time <10) {
+            System.out.println("Good morning");
+        } else if (time < 20) {
+            System.out.println("Good day");
+        } else {
+            System.out.println("Good night");
+        }
+
+        int btc = 9;
+        if (btc < 10) {
+            System.out.println("мало");
+        } else if (btc >=10 && btc <=99) {
+            System.out.println("среднее");
+        } else {
+            System.out.println("много");
+        }
+
+        char d = 'd';
+        String vowels = "aeiouAEIOU";
+        if(vowels.contains(Character.toString(d))){
+            System.out.println("гласная");
+        } else {
+            System.out.println("согласная");
+        }
+
+        int day = 3;
+        switch(day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            default:
+                System.out.println("Wednesday");
+        }
+        int i = 4;
+        int r = i%2;
+        switch (r){
+            case 0:
+                System.out.println("even");
+                break;
+            default:
+                System.out.println("odd");
+        }
+    }
 }
