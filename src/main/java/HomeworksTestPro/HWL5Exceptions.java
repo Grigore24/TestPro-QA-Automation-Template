a8bd4068450ed1161334ad33b9d1d7b0c6133b52
Grigore24 <133910092+Grigore24@users.noreply.github.com>
2026-04-18 23:43:09 +0100

    lecture5TryCatch

---
+++ src/main/java/HomeworksTestPro/HWL5Exceptions.java
@@ -0,0 +1,15 @@
+package HomeworksTestPro;
+
+public class HWL5Exceptions {
+    public static void main(String[] args) throws Exception {
+        /*String g = "hjkhfjkhsdkhfksfhs";
+        int size = g.length();
+        if (size > 10) {
+            throw new Exception("String is too long");
+        }*/
+        boolean isdecember = false;
+        if (isdecember == false){
+            throw new Exception("No christmas this month");
+        }
+    }
+}
